f823bc3344e5b8f044cf84d816494874ec3a8311
Tanner Lowthorp <lowthorpt@gmail.com>
2024-03-22 01:03:14 -0500

    getTile

--- src/main/java/rodeo/o2craft/Main.java
+++ src/main/java/rodeo/o2craft/Main.java
@@ -1,5 +1,6 @@
 package rodeo.o2craft;
 
+import rodeo.o2craft.GameElements.Graphics.Console.Board;
 import rodeo.o2craft.Input.ConsoleInput;
 import rodeo.o2craft.Input.TextInput;
 import rodeo.o2craft.Output.ConsoleOutput;
@@ -9,8 +10,10 @@ public class Main {
     public static void main(String[] args) {
         TextInput input = new ConsoleInput();
         TextOutput output = new ConsoleOutput();
+        Board board = new Board(8, 8);
 
         String userInput = input.getInput();
         output.printLine(userInput);
+        output.printLine(board.getTile(5,7));
     }
 }
